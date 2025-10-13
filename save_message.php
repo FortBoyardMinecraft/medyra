917a50d269c3f13e1e17618ebb3d2c4ceed169de
FortBoyardMinecraft <nathanthoma@outlook.fr>
2025-10-13 11:02:34 +0200

    Update save_message.php

--- save_message.php
+++ save_message.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $pseudo = htmlspecialchars($_POST['pseudo']);
     $message = htmlspecialchars($_POST['message']);
     $data = "[".date('Y-m-d H:i:s')."] $pseudo <$email>: $message\n";
-    file_put_contents('message.txt', $data, FILE_APPEND);
+    file_put_contents(__DIR__.'/message.txt', $data, FILE_APPEND);
     echo "Message enregistré";
 }
 ?>
